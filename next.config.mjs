32b11051aac1ddd2d0e994147bd8da8e5f90f7b9
OgabekYuldoshev <yuldashoff1@gmail.com>
2025-05-20 17:55:16 +0500

    fix: update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	transpilePackages: ["next-mdx-remote"],
-	outputFileTracingIncludes: {
-		"/blog/[slug]": ["./outstatic/**/*"],
-	},
+  reactStrictMode: true,
+  transpilePackages: ["next-mdx-remote"],
+  // outputFileTracingIncludes: {
+  // 	"/blog/[slug]": ["./outstatic/**/*"],
+  // },
 };
 
 export default nextConfig;
